a443bb706b448df543d19ee958da939cd2ebae49
yogyy <yogimr261@gmail.com>
2024-07-31 13:02:47 +0700

    Script Setup pattern

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1 @click="changeBio">my name is {{ name }}, I'm {{ age }} years old.</h1>
+  <Button @click-button="showAlert" :text="name" />
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+import Button from "./components/Button.vue";
+
+const name = ref("moretti");
+const age = ref(24);
+
+function changeBio() {
+  name.value = "klein";
+  age.value = 20;
+}
+
+const showAlert = () => alert("Child button was clicked");
+</script>
+
+<!-- <script lang="ts">
+import { ref } from "vue";
+import Button from "./components/Button.vue";
+
+export default {
+  setup() {
+    const name = ref("moretti");
+    const age = ref(24);
+
+    const changeBio = () => {
+      name.value = "klein";
+      age.value = 20;
+    };
+
+    const showAlert = () => alert("Child button was clicked");
+
+    return {
+      name,
+      age,
+      changeBio,
+      showAlert,
+    };
+  },
+  components: {
+    Button,
+  },
+};
+</script> -->
---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button @click="handleButtonClick">{{ text }}</button>
+</template>
+
+<script lang="ts" setup>
+const { text } = defineProps<{
+  text: string;
+}>();
+
+const emit = defineEmits(["clickButton"]);
+const handleButtonClick = () => emit("clickButton");
+</script>
+
+<!-- <script lang="ts">
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "No text specified",
+    },
+  },
+  emits: ["clickButton"],
+};
+</script> -->
