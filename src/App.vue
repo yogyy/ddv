dd56dce0f7c29c8155e1213945334156a07d220f
yogyy <yogimr261@gmail.com>
2024-08-04 00:39:21 +0700

    fix route

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<template>
-  <div class="flex gap-3 justify-center py-10">
-    <router-link to="/">Options</router-link>
-    <router-link to="/composition">Composition</router-link>
-  </div>
-  <router-view />
-</template>
+<template><RouterView /></template>
 
 <script lang="ts" setup></script>
--- src/components/todo/Composition.vue
+++ src/components/todo/Composition.vue
@@ -1,4 +1,8 @@
 <template>
+  <div class="flex gap-3 justify-center py-10">
+    <router-link to="/todos/options">Options</router-link>
+    <router-link to="/todos/composition">Composition</router-link>
+  </div>
   <div class="">
     <h3 class="text-lg font-semibold">You have {{ todosCount }} Todos!</h3>
     <input
